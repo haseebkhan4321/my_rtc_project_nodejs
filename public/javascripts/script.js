e78932d2d04cc0e0ec20c72fe12ebff271eca82a
Haseeb Khan <haseebkhan4321@gmail.com>
2021-03-23 22:01:25 +0500

    test

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,64 +1,74 @@
 var socket = io('/');
 var videoGrid = document.getElementById('video-grid');
-var myPeer = new Peer(undefined,{
-    host:"/",
-    port: '3001'
+var myPeer = new Peer(undefined, {
+    host: "/",
+    port: '3001',
+    config: {
+        icerServers: [{
+                url: 'stun:stun1.l.google.com:19302'
+            },
+            {
+                url: 'turn:numb.viagenie.ca',
+                credential: 'muazkh',
+                username: 'webrtc@live.com'
+            }
+        ]
+    }
 });
 var peers = {}
 var myVideo = document.createElement('video');
-myVideo.muted=true;
+myVideo.muted = true;
 navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true
-}).then(stream=>{
-    addVideoStream(myVideo,stream);
+}).then(stream => {
+    addVideoStream(myVideo, stream);
 
-    myPeer.on('call',call=>{
+    myPeer.on('call', call => {
         call.answer(stream);
         video = document.createElement('video');
-        call.on('stream',userVideoStream=>{
-            addVideoStream(video,userVideoStream);
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream);
         });
 
     })
 
-    socket.on('user-connected',userId=>{
-        connectToNewUser(userId,stream);
+    socket.on('user-connected', userId => {
+        connectToNewUser(userId, stream);
     });
-    
+
 });
 
 // Math.random()
-myPeer.on('open',id=>{
-    socket.emit('join-room',ROOM_ID,id);
-}); 
+myPeer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id);
+});
 
-function connectToNewUser(userId,stream){
-    const call = myPeer.call(userId,stream);
+function connectToNewUser(userId, stream) {
+    const call = myPeer.call(userId, stream);
     var video = document.createElement('video');
-    call.on('stream',userVideoStream=>{
-        addVideoStream(video,userVideoStream);
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream);
     });
-    call.on('close',()=>{
-         video.remove();
+    call.on('close', () => {
+        video.remove();
     })
-    peers[userId]=call
+    peers[userId] = call
 
 }
 
 
-function addVideoStream(video,stream){
- video.srcObject = stream;
-    video.addEventListener('loadedmetadata',()=>{
+function addVideoStream(video, stream) {
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', () => {
         video.play();
     });
     videoGrid.append(video);
 }
 
 
-socket.on('user-disconnected',userId=>{
-    if(peers[userId])
-    {
+socket.on('user-disconnected', userId => {
+    if (peers[userId]) {
         peers[userId].close();
     }
 })
--- public/javascripts/socket.js
+++ public/javascripts/socket.js
@@ -8,9 +8,7 @@ var peer = new Peer(undefined, {
 
 peer.on('open', user_id => {
     document.getElementById('my_user_id').innerHTML = user_id;
-    socket.on("user-added", (user_id) => {
-        console.log(user_id);
-    })
+
     navigator.mediaDevices.getUserMedia({
         video: true,
         audio: true
@@ -19,10 +17,27 @@ peer.on('open', user_id => {
         small_video.muted = true;
         small_video.srcObject = stream;
         socket.emit('user-connect', user_id);
+        peer.on('call', call => {
+            call.answer(stream);
+            call.on('stream', userVideoStream => {
+                small_video = document.getElementById("full-video");
+                small_video.muted = true;
+                small_video.srcObject = userVideoStream;
+            });
+
+        });
         document.getElementById('add_user').onclick = () => {
             call_user_id = document.getElementById('call_user').value;
-            console.log(call_user_id);
             socket.emit('add-user', user_id, call_user_id);
         }
+        socket.on("user-added", (user_id) => {
+            const call = peer.call(user_id, stream);
+            call.on('stream', userVideoStream => {
+                small_video = document.getElementById("full-video");
+                small_video.muted = true;
+                small_video.srcObject = userVideoStream;
+            });
+        })
+
     });
 });
